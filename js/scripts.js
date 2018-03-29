de1233d4d8808ae882cfc6889b615332b4c0166a
gracekashe <gracekashe@gmail.com>
2018-03-29 12:15:23 +0300

    new images downloaded and included a backend and front face interface

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+//back-end logic//
+var pPong = [];
+
+function pingpong(num) {
+  for (var x = 1; x <= num; x++) {
+
+    if (x % 3 == 0) {
+      pPong.push('ping');
+
+    } else if (x % 5 == 0) {
+      pPong.push('pong');
+
+    } else if ((x % 5 == 0) && (x % 3 == 0)) {
+      pPong.push('pingpong');
+
+    } else {
+      pPong.push(x);
+    }
+
+  };
+
+};
+
+
+
+
+
+//user interface logic//
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    var result = parseInt($("#anyNumber").val());
+    pingpong(result);
+    console.log(pPong);
+
+    pPong.forEach(function(e) {
+      $(".outPut").append($("<li>").html(e));
+    })
+
+
+
+
+    // alert("hey");
+  });
+
+
+
+
+});
